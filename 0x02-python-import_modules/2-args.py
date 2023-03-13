831af48a831194cebc85c5f3691d895249f592b6
Zumji <ambert24real@gmail.com>
2023-03-13 12:15:47 -0700

    argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 if __name__ == " __main__":
     import sys
+
     count = len(sys.argv) - 1
     if count == 0:
-        print("0 argguments.")
+        print("0 arguments.")
     elif count == 1:
         print("1 argument:")
     else:
