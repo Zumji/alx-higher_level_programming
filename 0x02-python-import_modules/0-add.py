e966ded6c161f4eedd268bc1f37ae678cb02b67c
Zumji <ambert24real@gmail.com>
2023-03-13 02:19:21 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__:"
-from add_0 import add
+import add
 a = 1
 b = 2
-print("{:02} + {:02} = {:02}".format(a, b, add(a, b)))
+print("{:02} + {:02} = {:02}i.format(a, b, add(a, b)))
