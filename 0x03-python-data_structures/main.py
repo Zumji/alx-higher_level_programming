3aee19b126acc9a7abc67bb43bdec27d6795dc16
Zumji <ambert24real@gmail.com>
2023-03-16 00:47:42 -0700

    0-print list

---
+++ 0x03-python-data_structures/main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+print_revsersed_list_integer = _import__
+('3-print_revsersed_list_integer').print_reversed_list_integer
+my_list = [1, 2, 3]
+print_reversed_list_integer(my_list)
