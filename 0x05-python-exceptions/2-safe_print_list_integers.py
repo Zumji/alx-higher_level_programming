3203e6cfa95b50e1af0940d32ecc7870c14dcc8c
Zumji <ambert24real@gmail.com>
2023-04-20 02:00:58 -0700

    safe print

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,10 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     ret = 0
     for i in range (0, x):
-        try: 
+        try:
             print("{:d}".format(my_list[i]), end="")
             ret += 1
         except ValueError:
-            continue 
+            continue
     print("")
     return (ret)
