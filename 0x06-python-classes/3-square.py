4d620151cc2a326440c1907cc327828a0e77af39
Zumji <ambert24real@gmail.com>
2023-04-21 02:01:21 -0700

    3 square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """ class Square that defines a square"""
 
 
 class Square:
     """ class Square that defines a square"""
+
     def __init__(self, size=0):
         """initialize square
         Args:
@@ -15,6 +17,7 @@ class Square:
             raise ValueError('size must be >= 0')
         else:
             self.__size = size  #: size of the square
+
     def area(self):
         """returns the area.
         Returns:
