a6f25c33d9416346d9a839167f8de8803657ec96
Zumji <ambert24real@gmail.com>
2023-04-21 01:17:52 -0700

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -7,8 +7,9 @@ class Square:
     """Represents a square"""
 
     def __init__(self, size):
-            """Initializing this square class
-            Args: size - represnets the size of the square defined
-            """
+        """Initializing this square class
+        Args: 
+            size - represnets the size of the square defined
+         """
 
-            self.__size = size
+        self.__size = size
